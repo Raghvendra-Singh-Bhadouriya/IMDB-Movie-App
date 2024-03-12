c21048c251cb95123eb445c7a1e10196a5e1002a
Raghvendra-Singh-Bhadouriya <raghvendrabhadouriya2003@gmail.com>
2024-03-12 18:02:31 +0530

    added movie app

---
+++ movie-app/src/Body/Allwebseries/Ontheairweb.jsx
@@ -0,0 +1,62 @@
+import "./Ontheairweb.css"
+import {Link} from "react-router-dom"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function Ontheairweb(){
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchOntheairwebData()
+    },[])
+
+    async function fetchOntheairwebData(){
+        try {
+            let res = await axios({
+                method: "get",
+                url:`https://api.themoviedb.org/3/tv/airing_today?api_key=68ae3ab018e32aad2fa3e1a4b51004cf`,
+            })
+            setData(res.data.results);
+            console.log(res.data.results);
+        } catch (error) {
+            console.error("Error fetching data:", error)
+        }
+    }
+
+    const settings = {
+        dots:false,
+        infinite:true,
+        speed:500,
+        autoplay:false,
+        slidesToScroll:4,
+        slidesToShow:4,
+    }
+
+    return(
+        <>
+        <div className="Ontheairweb-text-container">
+            <p className="Ontheairweb-text">On The Air</p>
+        </div>
+        <div className="Ontheairweb-main-container">
+            <div className="Ontheairweb-slider-container">
+                <Slider {...settings}>
+                    {data.map((Ontheairweb) => {
+                        return(
+                        <>
+                            <div key={Ontheairweb.id} className="Ontheairweb-poster-container">
+                                <Link to={`/webdetails/${Ontheairweb.id}`}>
+                                    <img className="Ontheairweb-poster-img" src={`https://image.tmdb.org/t/p/w500/${Ontheairweb.poster_path}`}/>
+                                </Link>
+                            </div>
+                        </>
+                        )
+                    })}
+                </Slider>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ movie-app/src/Body/Allwebseries/Popularwebseries.jsx
@@ -0,0 +1,64 @@
+import "./Popularwebseries.css"
+import {Link} from "react-router-dom"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
+
+export default function Popularwebseries(){
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetchPopularwebseriesData()
+    },[])
+
+    async function fetchPopularwebseriesData(){
+        try {
+            let res = await axios({
+                method:"get",
+                url:`https://api.themoviedb.org/3/tv/popular?api_key=68ae3ab018e32aad2fa3e1a4b51004cf`,
+            })
+            setData(res.data.results);
+            console.log(res.data.results);
+        } catch (error) {
+            console.error("Error fetching data:", error)
+        }
+    }
+
+    const settings ={
+        dots:false,
+        infinite:true,
+        speed:500,
+        autoplay:false,
+        slidesToScroll:4,
+        slidesToShow:4,
+    }
+
+    return(
+        <>
+        <div className="popularweb-text-container">
+            <p className="popularweb-text">Popular WebSeries</p>
+        </div>
+        <div className="popularweb-main-container">
+            <div className="popularweb-slider-container">
+                <Slider {...settings}>
+                    {data.map((popularweb) => {
+                        return(
+                        <>
+                            <div key={popularweb.id} className="popularweb-poster-container">
+                                <Link to={`/webdetails/${popularweb.id}`}>
+                                    <img className="popularweb-poster-img" src={`https://image.tmdb.org/t/p/w500/${popularweb.poster_path}`}/>
+                                </Link>
+                            </div>
+                        </>
+                        )
+                    })}
+                </Slider>
+            </div>
+        </div>
+        </>
+    )
+}
